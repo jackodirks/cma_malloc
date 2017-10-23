3e0e9179667d4347ad8a61fc9433530948d0430e
Jacko Dirks <jacko.dirks@gmail.com>
2017-10-23 14:16:54 +0200

    test.c: Fixed print and made reqSize smaller

--- userspace/test.c
+++ userspace/test.c
@@ -5,7 +5,7 @@
 #include "contiguousMalloc.h"
 
 int main(){
-    size_t reqSize = 1.5*pow(2, 27); //192 MiB
+    size_t reqSize = pow(2, 26); //64 MiB
     uintptr_t phys_addr;
     void* userspace = mallocContiguous(reqSize, &phys_addr);
     if (userspace == NULL){
@@ -13,8 +13,8 @@ int main(){
         exit(EXIT_FAILURE);
     }
     printf("Aquired %zu MiB memory!\n", (size_t)(reqSize/pow(2, 20)));
-    printf("\tPhysical address: 0x%p\n", (void*)phys_addr);
-    printf("\tUserspace address: 0x%p\n", userspace);
+    printf("\tPhysical address: %p\n", (void*)phys_addr);
+    printf("\tUserspace address: %p\n", userspace);
     if (freeContiguous(phys_addr, userspace, reqSize) != 0){
         printf("Failed to free memory!\n");
         exit(EXIT_FAILURE);
